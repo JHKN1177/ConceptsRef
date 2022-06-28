13d7566f82048f8e1326f8676ed51af4d67c95ca
JHKN1177 <95961931+JHKN1177@users.noreply.github.com>
2022-06-28 19:11:53 +0530

    Update graph01.ps1

--- graph01.ps1
+++ graph01.ps1
@@ -4,7 +4,7 @@
 
 
 # Store the query in a variable
-$query = 'Resources | project name, type | order by name asc | limit 5'
+$query = 'Resources | project name, type | order by name asc | limit 10'
 
 # Run Azure Resource Graph query with `order by` first, then with `limit`
 Search-AzGraph -Query $query
